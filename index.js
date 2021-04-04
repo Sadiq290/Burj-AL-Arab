8e2538437c4841c460edfa9d0620c0d3d4349d47
Sadiq <sadiqmumu27@gmail.com>
2021-04-05 01:09:34 +0600

    Hello

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get('/', (req, res) => {
 })
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u16ta.mongodb.net/BurjAlArab?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u16ta.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const bookings = client.db("BurjAlArab").collection("Booking");
+    const bookings = client.db(`${process.env.DB_NAME}`).collection("Booking");
     app.post('/addBooking', (req, res) => {
         const newBooking = req.body
         bookings.insertOne(newBooking)
